b5e564ad5122d6acbe7b217e0bb785d5a0a157eb
retur <return0fthekingv@gmail.com>
2024-07-29 16:20:58 +0300

    Solution

--- cypress/e2e/hermioneAccount.cy.js
+++ cypress/e2e/hermioneAccount.cy.js
@@ -1,9 +1,14 @@
+import { faker } from '@faker-js/faker';
 /// <reference types='cypress' />
 
 describe('Bank app', () => {
   const user = 'Hermoine Granger';
   const accountNumber = '1001';
   const accountNumber2 = '1002';
+  const depositAmount = faker.number.int({ min: 500, max: 1000 });
+  const withdrawAmount = faker.number.int({ min: 50, max: 500 });
+  const initialBalance = 5096;
+  // const balance = `${depositAmount - withdrawAmount + initialBalance}`;
   before(() => {
     cy.visit('/');
   });
@@ -17,34 +22,36 @@ describe('Bank app', () => {
       .contains('strong', accountNumber)
       .should('be.visible');
     cy.contains('[ng-hide="noAccount"]', 'Balance')
-      .contains('strong', '5096')
+      .contains('strong', initialBalance)
       .should('be.visible');
     cy.contains('.ng-binding', 'Dollar')
       .should('be.visible');
 
     cy.get('[ng-class="btnClass2"]').click();
-    cy.get('.form-control').type('1000');
+    cy.get('.form-control').type(depositAmount);
     cy.get('form.ng-dirty > .btn').click();
     cy.get('.error').should('contain', 'Deposit Successful');
     cy.contains('[ng-hide="noAccount"]', 'Balance')
-      .contains('strong', '6096');
+      .contains('strong', initialBalance + depositAmount);
     cy.get('[ng-class="btnClass3"]').click();
     cy.contains('[type="submit"]', 'Withdraw')
       .should('be.visible');
-    cy.get('[placeholder="amount"]').type('5096');
+    cy.get('[placeholder="amount"]').type(withdrawAmount);
     cy.get('form.ng-dirty > .btn').click();
 
     cy.get('.error').should('contain', 'Transaction successful');
     cy.contains('[ng-hide="noAccount"]', 'Balance')
-      .contains('strong', '1000')
+      .contains('strong', (initialBalance + depositAmount) - withdrawAmount)
       .should('be.visible');
-
+    // eslint-disable-next-line cypress/no-unnecessary-waiting
+    cy.wait(1000);
     cy.get('[ng-click="transactions()"]').click();
-    // cy.get('#anchor0 > :nth-child(1)').should('be.visible');
+    cy.get('#anchor0 > :nth-child(1)').should('be.visible');
     cy.get('#start').type('2024-07-28T00:00');
-    cy.get('#anchor0 > :nth-child(2)').should('contain', '1000');
+    cy.get('#anchor0', { timeout: 10000 }).should('be.visible');
+    cy.get('#anchor0 > :nth-child(2)').should('contain', depositAmount);
     cy.get('#anchor0 > :nth-child(3)').should('contain', 'Credit');
-    cy.get('#anchor1 > :nth-child(2)').should('contain', '5096');
+    cy.get('#anchor1 > :nth-child(2)').should('contain', withdrawAmount);
     cy.get('#anchor1 > :nth-child(3)').should('contain', 'Debit');
     cy.get('.fixedTopBox > [style="float:left"]').click();
     cy.get('#accountSelect').select(accountNumber2);
